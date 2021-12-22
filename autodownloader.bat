7d56e5dd9f84f1dc75083004aeb34514ae0130d3
FOREVEREALIZE JB <felixcrat@gmail.com>
2021-12-22 19:11:55 +0100

    Things done

--- autodownloader.bat
+++ autodownloader.bat
@@ -1,11 +1,10 @@
-powershell
 cd C:
 mkdir temp
 cd temp
 mkdir intrudoor
 cd intrudoor
-Invoke-WebRequest https://github.com/FOREVEREALIZE/Thingies/raw/main/IntruDoor-1.0-SNAPSHOT.zip -O IntruDoor.zip
-Expand-Archive -Force IntruDoor.zip .
+curl https://github.com/FOREVEREALIZE/Thingies/raw/main/IntruDoor-1.0-SNAPSHOT.zip -O IntruDoor.zip
+powershell -command "Expand-Archive -Force IntruDoor.zip ."
 cd IntruDoor-1.0-SNAPSHOT
 cd bin
 ./IntruDoor.bat
